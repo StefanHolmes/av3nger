20e4462e05434d0f869402b6ced52b1af01e7084
Stefan Holmes <stefan.c.holmes@gmail.com>
2021-02-04 19:17:47 +0000

    Can actually load in assets now

--- includes.asm
+++ includes.asm
@@ -1,4 +1,8 @@
-.var font = LoadBinary("font_44x8.bin")
+.var font = LoadBinary("assets\font_44x8.bin")
+.var enemies = LoadBinary("assets\sprites_48x8.bin")
 
 * = $2000 "Font graphics"
 fontData: .fill font.getSize(), font.get(i)
+
+* = $2160 "Enemy graphics"
+enemyData: .fill enemies.getSize(), enemies.get(i)
--- main.vs
+++ main.vs
@@ -1,4 +1,5 @@
 al C:c000 .init
+al C:2160 .enemyData
 al C:c021 .clear
 al C:80c .upstartEnd
 al C:2000 .fontData
