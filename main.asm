a1311fa7a1ac91ba68098372c6b17eb7860a1e88
Stefan Holmes <stefan.c.holmes@gmail.com>
2021-03-21 16:43:13 +0000

    Tidied code with KickAssembler for loop

--- main.asm
+++ main.asm
@@ -38,38 +38,10 @@ fullclear:
         txa     // X register is zero because of the previous loop.
         sei     // Turn off interrupts
 
-!loop:  sta $2000,x
-        sta $2000+($100*1),x
-        sta $2000+($100*2),x
-        sta $2000+($100*3),x
-        sta $2000+($100*4),x
-        sta $2000+($100*5),x
-        sta $2000+($100*6),x
-        sta $2000+($100*7),x
-        sta $2000+($100*8),x
-        sta $2000+($100*9),x
-        sta $2000+($100*10),x
-        sta $2000+($100*11),x
-        sta $2000+($100*12),x
-        sta $2000+($100*13),x
-        sta $2000+($100*14),x
-        sta $2000+($100*15),x
-        sta $2000+($100*16),x
-        sta $2000+($100*17),x
-        sta $2000+($100*18),x
-        sta $2000+($100*19),x
-        sta $2000+($100*20),x
-        sta $2000+($100*21),x
-        sta $2000+($100*22),x
-        sta $2000+($100*23),x
-        sta $2000+($100*24),x
-        sta $2000+($100*25),x
-        sta $2000+($100*26),x
-        sta $2000+($100*27),x
-        sta $2000+($100*28),x
-        sta $2000+($100*29),x
-        sta $2000+($100*30),x
-        
+!loop:
+.for(var i=$100*30;-1<i;i=i-$100) {
+        sta $2000+i,x
+}
         dex
         bne !loop-
 
@@ -85,39 +57,13 @@ fullclear:
 // TODO: #3 This isn't part of the attract mode. Move it to be called when player starts
 drawBottomLine:
         lda #$ff
-        sta $3e07,x
-        sta $3e07+$08,x
-        sta $3e07+$10,x
-        sta $3e07+$18,x
-        sta $3e07+$20,x
-        sta $3e07+$28,x
-        sta $3e07+$30,x
-        sta $3e07+$38,x
-        sta $3e07+$40,x
-        sta $3e07+$48,x
-        sta $3e07+$50,x
-        sta $3e07+$58,x
-        sta $3e07+$60,x
-        sta $3e07+$68,x
-        sta $3e07+$70,x
-        sta $3e07+$78,x
-        sta $3e07+$80,x
-        sta $3e07+$88,x
-        sta $3e07+$90,x
-        sta $3e07+$98,x
-        sta $3e07+$a0,x
-        sta $3e07+$a8,x
-        sta $3e07+$b0,x
-        sta $3e07+$b8,x
-        sta $3e07+$c0,x
-        sta $3e07+$c8,x
-        sta $3e07+$d0,x
-        sta $3e07+$d8,x
-                
+.for(var i=$d8;-1<i;i=i-8) {
+        sta $3e07+i,x
+        }
+
         cli     // Turn interrupts back on
         rts
 
 #import "graphics.asm"
 
-text:
 #import "text.asm"
